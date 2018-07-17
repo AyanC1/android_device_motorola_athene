f0971579e65ad1a753c0d4331e1d9d69592e34a4
AyanC1 <choudhury.ayan13@gmail.com>
2018-07-17 03:48:02 +0000

    adding official tag and removing stock camera

--- fh.mk
+++ fh.mk
@@ -3,6 +3,12 @@ $(call inherit-product, device/motorola/athene/fh_athene.mk)
 # Inherit some common FH stuff.
 $(call inherit-product, vendor/fh/config/common_full_phone.mk)
 
+#disabling the stock camera
+DISABLE_CNM := true
+
+#making it an official build
+FH_RELEASE=true
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := fh_athene
 PRODUCT_RELEASE_NAME := athene
